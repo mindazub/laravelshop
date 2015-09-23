4e6dd2e63e20c8c68da515acaa195e07b672a11e
vagrant <vagrant@homestead>
2015-09-23 10:34:30 +0000

    initial commit. no checkout

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'password' => bcrypt(str_random(10)),
+        'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        // 'description' => $faker->sentence(3),
+        'price' => $faker->numberBetween(10,20),
+        // 'imageurl' =>  $faker->file($sourceDir = '/first', $targetDir = '/second', false),
+        // 'file_id' => $faker->numberBetween($min = 1, $max = 10),
+    ];
+});
---
+++ database/migrations/2015_09_23_080553_create_files_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('files', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('filename');
+            $table->string('mime');
+            $table->string('original_filename');
+            $table->timestamps();
+        });
+    }
+ 
+    public function down()
+    {
+        Schema::drop('files');
+    }
+}
